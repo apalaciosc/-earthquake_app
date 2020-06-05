c6771bc3315f752db313930dc06e9a3bb2f2fdf6
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2020-06-04 23:47:28 -0500

    ADD: Eartqueake Map with conditions

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <ion-app>
+    <ion-content>
+      <DrawMap class="map" />
+    </ion-content>
+  </ion-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import DrawMap from "./components/DrawMap";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
+  components: {
+    DrawMap
+  }
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss" scoped>
+.map {
+  height: 425px;
 }
 </style>
